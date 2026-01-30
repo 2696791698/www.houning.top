7e7efd6ecef1b187f4322636b2f3b3e26347c3ac
Houning_YA <76392624+2696791698@users.noreply.github.com>
2026-01-31 06:11:16 +0800

    Update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="flex flex-col justify-center items-center">
+    <div class="text-neutral-400">
+      <a
+        href="https://beian.miit.gov.cn"
+        target="_blank"
+        class="hover:underline">「闽ICP备2025116672号-1」</a>
+    </div>
     <div class="text-neutral-400">Copyright © 2025 - 2026 Houning. All Rights Reserved.</div>
     <div class="text-neutral-400">
       本站内容如无特别声明, 均使用
@@ -9,12 +15,6 @@
         class="hover:underline"
       >CC BY-NC-ND 4.0</a>
       许可协议, 转载请注明出处.</div>
-    <div class="text-neutral-400">
-      <a
-        href="https://beian.miit.gov.cn"
-        target="_blank"
-        class="hover:underline">「闽ICP备2025116672号-1」</a>
-    </div>
     <br>
   </div>
 </template>
