c283b6c8b774be2f38a1d79e1253dde68b73d4e7
Houning_YA <76392624+2696791698@users.noreply.github.com>
2026-02-21 16:11:54 +0800

    Update

---
+++ app/app.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { data } = await useFetch('/api/posts')
+</script>
+
+<template>
+  <div>
+    <h1>文章列表</h1>
+    <ul>
+      <li v-for="post in data" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
+</template>
